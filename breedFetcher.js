06fb1bb1bc83baac94463056cad6f66be63c14d4
Mitchell Aldrich <mitch.w.aldrich@gmail.com>
2021-07-01 23:04:42 +0000

    refactored breedFetcher to run out of index.js, and implemented test from breedFetcherTest.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,18 @@
-const args = process.argv.slice(2);
 const request = require('request');
+const args = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args}`, (error, response, body) => {
-  const data = JSON.parse(body);
-  if (error) {
-    console.log('error:', error); // Print the error if one occurred
-    return;
-  }
-  if (data.length === 0) {
-    console.log('No cats found');
-    return;
-  }
-  console.log(`first entry: ${data[0].description}`);
-});
+const fetchBreedDescription = (args, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${args}`, (error, response, body) => {
+    const data = JSON.parse(body);
+    if (error) {
+      return callback(error);
+    }
+    if (data.length === 0) {
+      return callback(error);
+    }
+    let desc = data[0].description;
+    return callback(null, desc);
+  });
+};
+
+module.exports = { fetchBreedDescription };
